aa60fbe0383e368b1d2a61bae5533c5c0adcd180
Nikita Smolenskyi <57872055+NikitaArd@users.noreply.github.com>
2024-03-10 15:30:33 +0100

    feature/creating "Learn More" page (#5)
    * feature: created homepage with Start Quiz and Learn More buttons
    
    * feature: created learn-more template file
    
    * feature: created view function to serve learn-more page
    
    * feature: filled learn-more page with temporary text to test and added learn-more url to main app urls
    
    * fix: added learn-more page url to the homepage and fixed app_name in main app urls to provide named routing
    
    * feature: added todo task to views, wrote first article on learn-more page and imported appropriate image
    
    * feature: created video article on learn-more page
    
    * feature: wrote article about sources of global co2 emissions and imoported appropriate image
    
    * feature: wrote article about ways to redurece co2 emissions and imported appropriate image
    
    * feature: created footer and fill with homepage quiz and contact links
    
    * feature: created header of the page
    
    * feature: added and styled breadcrumb ( doesnt work yet )
    
    * fix: added work links to breadcrumb, moved breadcrumb on top of page and added quote in header of the page

--- eco_trace/main/urls.py
+++ eco_trace/main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from .views import (
-    index
+    index,
+    learn_more
 )
 
+app_name = 'main'
 urlpatterns = [
     path('', index, name='index'),
+    path('learn-more/', learn_more, name='learn-more'),
 ]
--- eco_trace/main/views.py
+++ eco_trace/main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
+    # TODO: May be interesting to add popover with tips or interesting information about CO2 emissions to buttons. User hover the Start Quiz button after that popover shows tip or interesting info.
     return render(request, 'index.html')
+
+def learn_more(request):
+    return render(request, 'learn-more.html')
