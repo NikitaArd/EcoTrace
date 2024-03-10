67f64c81189a2e13623c9336a8fbec15ec4586cd
NikitaArd <11nik11255@gmail.com>
2024-03-10 14:06:47 +0100

    feature: added todo task to views, wrote first article on learn-more page and imported appropriate image

--- eco_trace/main/views.py
+++ eco_trace/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
+    # TODO: May be interesting to add popover with tips or interesting information about CO2 emissions to buttons. User hover the Start Quiz button after that popover shows tip or interesting info.
     return render(request, 'index.html')
 
 def learn_more(request):
