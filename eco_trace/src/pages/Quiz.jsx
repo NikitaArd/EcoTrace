53fde482bb133b18d6037d36384e2170168143dd
NikitaArd <11nik11255@gmail.com>
2024-07-01 20:11:14 +0200

    feat: created extra ui components for Quiz page (progress and button)

--- eco_trace/src/pages/Quiz.jsx
+++ eco_trace/src/pages/Quiz.jsx
@@ -1,13 +1,26 @@
 import Bike from "../assets/Bike.svg";
 
-import { RadioGroup } from "@chakra-ui/react";
+import { RadioGroup, Progress, Button } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 import QuizOption from "../components/QuizOption";
 
 export default function Quiz() {
+  const currentQuestion = 3;
+
   return (
     <div className="flex w-screen h-screen bg-transparent">
-      <section className="m-auto h-fit w-fit p-28 border-2 border-green-500 rounded-3xl shadow-xl">
+      <section className="m-auto h-fit w-fit p-28 pt-12 border-2 border-stone-200 rounded-sm shadow-xl">
+        <div className="mt-3 mb-20">
+          <p className="my-2 font-semibold">{currentQuestion} / 10</p>
+          <Progress
+            colorScheme="green"
+            size="sm"
+            value={currentQuestion * 10}
+            mt="0"
+          />
+        </div>
+
         <h1 className="text-3xl font-bold mx-4 my-5">
           How often do you use a car?
         </h1>
@@ -16,13 +29,23 @@ export default function Quiz() {
           <ul className="mt-7">
             <QuizOption value="a">Daily</QuizOption>
 
-						<QuizOption value="b">A few times a week</QuizOption>
+            <QuizOption value="b">A few times a week</QuizOption>
 
-						<QuizOption value="c">Rarely</QuizOption>
+            <QuizOption value="c">Rarely</QuizOption>
 
-						<QuizOption value="d">Never</QuizOption>
+            <QuizOption value="d">Never</QuizOption>
           </ul>
         </RadioGroup>
+
+        <Button
+          rightIcon={<ArrowForwardIcon />}
+          colorScheme="green"
+          variant="solid"
+					mt="8"
+					ml="70%"
+        >
+          Submit
+        </Button>
       </section>
       <img class="absolute max-h-56 ml-32 bottom-0 right-72" src={Bike} />
     </div>
