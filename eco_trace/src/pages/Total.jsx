78f1c79a9bc27f1bd2ab98dd2f24fdc0e7db070f
NikitaArd <11nik11255@gmail.com>
2024-07-05 18:29:28 +0200

    feat: generated function that returns total-dependent facts of score and styled Total page

--- eco_trace/src/pages/Total.jsx
+++ eco_trace/src/pages/Total.jsx
@@ -1,9 +1,25 @@
+import { Text } from '@chakra-ui/react'
+
+import get_fact from "../facts";
+
+
 export default function Total() {
-	const total = localStorage.getItem('total');
+  const total = localStorage.getItem("total");
+  const [fact, color] = get_fact(total);
+
+	console.log(color);
 
   return (
-    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black-600 md:text-5xl lg:text-6xl dark:text-white">
-      Your estimated CO2 trace is ~ <span class="text-gray-400">{total}</span> kg.
-    </h1>
+    <div className="h-screen w-screen pt-64">
+      <h1 className="mb-4 text-center text-4xl font-extrabold tracking-tight leading-none text-gray-800 md:text-5xl lg:text-6xl dark:text-white">
+        Your estimated CO<sub>2</sub> trace is ~<Text color={color}>{total}</Text> kg.
+      </h1>
+
+      <h2 className="font-bold text-2xl w-1/2 m-auto my-12 underline">Did you know ?</h2>
+
+      <p className='w-1/3 m-auto text-xl text-center'>
+       <span className='underline underline-offset-2'>{total}</span> kg C2<sub>2</sub> is {fact}
+      </p>
+    </div>
   );
 }
